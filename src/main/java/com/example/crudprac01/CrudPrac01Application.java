955674e5f20deaf290c93dc6c1b3a07bdb3f9958
4realavenue <4realdane@gmail.com>
2025-12-16 17:21:40 +0900

    CURD Practice Project

---
+++ src/main/java/com/example/crudprac01/CrudPrac01Application.java
@@ -0,0 +1,13 @@
+package com.example.crudprac01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudPrac01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudPrac01Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/crudprac01/repository/a.java
@@ -0,0 +1,4 @@
+package com.example.crudprac01.repository;
+
+public class a {
+}
