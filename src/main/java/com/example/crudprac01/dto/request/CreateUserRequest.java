cdaa7e0ca5c2e59c314bef83d0cb92adf94d6a42
4realavenue <4realdane@gmail.com>
2025-12-18 08:34:47 +0900

    💄 style: User 생성 시 JsonProperty 활용
    - 생성 시 받는 request에 JsonProperty로 이름 명시

--- src/main/java/com/example/crudprac01/dto/request/CreateUserRequest.java
+++ src/main/java/com/example/crudprac01/dto/request/CreateUserRequest.java
@@ -1,13 +1,20 @@
 package com.example.crudprac01.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.time.LocalDateTime;
 
 public class CreateUserRequest {
+
     private String name;
     private String email;
     private String password;
 
-    public CreateUserRequest(String name, String email, String password) {
+    @JsonCreator
+    public CreateUserRequest(@JsonProperty("name") String name,
+                             @JsonProperty("email") String email,
+                             @JsonProperty("password") String password) {
         this.name = name;
         this.email = email;
         this.password = password;
