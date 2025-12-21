5a25939d19139e2ecf071838f615a7a25cf1511e
4realavenue <4realdane@gmail.com>
2025-12-21 22:21:09 +0900

    ✨ feat: CrudPrac01 - Member
    crud 생성

---
+++ src/main/java/com/example/crudprac01/entity/Member.java
@@ -0,0 +1,4 @@
+package com.example.crudprac01.entity;
+
+public class Member {
+}
--- src/main/java/com/example/crudprac01/repository/SongRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.crudprac01.repository;
-
-import com.example.crudprac01.entity.Song;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SongRepository extends JpaRepository <Song, Long> {
-}
